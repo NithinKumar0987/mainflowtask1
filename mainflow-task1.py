8a885232fe3eee4804ebb919dcff0f185baee877
NithinKumar0987 <148206414+NithinKumar0987@users.noreply.github.com>
2024-05-02 17:59:39 +0530

    Add files via upload

---
+++ mainflow-task1.py
@@ -0,0 +1,41 @@
+# Create a list
+list = [10, 20, 30, 40, 50]
+
+# Add an element to the list
+list.append(60)
+
+# Remove an element from the list
+list.remove(20)
+
+# Modify an element in the list
+list[1] = 80
+
+# Print the updated list
+print("List:", list)
+
+# Create a dictionary
+my_dict = {'name': 'Ali', 'age': 18, 'city': 'vizag'}
+
+# Add a key-value pair to the dictionary
+my_dict['occupation'] = 'Engineer'
+
+# Remove a key from the dictionary
+del my_dict['age']
+
+# Modify a value in the dictionary
+my_dict['city'] = 'Los Angeles'
+
+# Print the updated dictionary
+print("Dictionary:", my_dict)
+
+# Create a set
+my_set = {10, 20, 30, 40, 50}
+
+# Add an element to the set
+my_set.add(60)
+
+# Remove an element from the set
+my_set.discard(30)
+
+# Print the updated set
+print("Set:", my_set)
